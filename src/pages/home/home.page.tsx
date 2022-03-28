f68151eb4e2707724fb1ef9ffdf8d8bcc4f30f98
Efen Stakes <efenstakes101@gmail.com>
2022-03-28 14:11:51 +0300

    add app pages skeleton page layout

---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,12 @@
+import './home.page.scss'
+
+
+const HomePage = () => {
+  return (
+    <div className='page'>
+        
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/profile/profile.page.tsx
@@ -0,0 +1,11 @@
+import './profile.page.scss'
+
+const ProfilePage = () => {
+  return (
+    <div className='page'>
+        
+    </div>
+  )
+}
+
+export default ProfilePage
