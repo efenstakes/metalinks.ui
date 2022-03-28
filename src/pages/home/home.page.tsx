74f29d0da306a0eca11bd33e2bda9d536014f86a
Efen Stakes <efenstakes101@gmail.com>
2022-03-28 16:28:58 +0300

    add metalinks to home page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,27 +1,52 @@
 // components
 import AppbarComponent from '../../components/appbar/appbar.component'
-
-
-import './home.page.scss'
+import MetalinkCardComponent from '../../components/metalink_card/metalink_card.component'
+import SectionTitleComponent from '../../components/section_title/section_title.component'
 import WelcomeComponent from './welcome.component'
 
 
+// models
+import { Avatar } from '../../models/avatar.model'
+import { MetaLink } from '../../models/metalink.model'
+import { avatars } from '../../models/test.data'
+
+
+import './home.page.scss'
+
+
 const HomePage = () => {
-  return (
-    <div className='page'>
-        
-        {/* appbar */}
-        <AppbarComponent />
 
-        {/* welcome */}
-        <WelcomeComponent />
+    console.log("avatars ", avatars)
 
-        {/* metalinks if any */}
+    return (
+        <div className='page'>
+            
+            {/* appbar */}
+            <AppbarComponent />
 
-        {/* add link button */}
+            {/* welcome */}
+            <WelcomeComponent />
 
-    </div>
-  )
+            {/* metalinks if any */}
+            <SectionTitleComponent title='Links' />
+            <div className="padded_container">
+                {
+                    avatars[0].links.map((metaLink: MetaLink, index: number)=> {
+
+                        return (
+                            <MetalinkCardComponent
+                                key={index}
+                                metaLink={metaLink}
+                            />
+                        )
+                    })
+                }
+            </div>
+
+            {/* add link button */}
+
+        </div>
+    )
 }
 
 export default HomePage
