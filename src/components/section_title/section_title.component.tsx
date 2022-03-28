1770dde730a989f38885e4b754ea63ddbe7f85ed
Efen Stakes <efenstakes101@gmail.com>
2022-03-28 14:26:35 +0300

    add section title component

---
+++ src/components/section_title/section_title.component.tsx
@@ -0,0 +1,20 @@
+import './section_title.component.scss'
+
+type ComponentProps = {
+    title: string,
+    children?: any,
+}
+const SectionTitleComponent = ({ title, children }: ComponentProps) => {
+  return (
+    <div className='padded_container_sm section_title_container'>
+
+        <p className='section_title_container__title su_3'> { title } </p>
+        
+        <div className='section_title_container__right su_5'>
+            { children }
+        </div>
+    </div>
+  )
+}
+
+export default SectionTitleComponent
