35a13c1c08d690071ab28b0868c42a2602b2888c
Efen Stakes <efenstakes101@gmail.com>
2022-03-28 16:28:32 +0300

    add metalink card component

---
+++ src/components/metalink_card/metalink_card.component.tsx
@@ -0,0 +1,57 @@
+// models
+import { MetaLink } from '../../models/metalink.model'
+import VSpacerComponent from '../v_spacer/v_spacer.component'
+
+
+// styles
+import './metalink_card.component.scss'
+
+
+
+type ComponentProps = {
+  metaLink: MetaLink
+}
+const MetalinkCardComponent = ({ metaLink }: ComponentProps) => {
+  return (
+    <div className='metalink_card paper_sm clickable row'>
+
+      {/* image */}
+      <div className="metalink_card__image_container">
+        <img 
+          src={metaLink.avatar} 
+          alt="avatar" 
+          className="metalink_card__image_container_image" 
+        />
+      </div>
+
+      {/* content */}
+      <div className="metalink_card__content_container column ma_center">
+
+        <div className="metalink_card__content_container__title row ma_space_btn ca_center">
+
+          <p className="text_5 bold">
+            { metaLink.name }
+          </p>
+
+          <div className="chip metalink_card__content_container__title__chip text_6 bold">
+            { metaLink.is_active ? 'Active' : 'Inactive' }
+          </div>
+
+        </div>
+
+        <p className="text_7 bold">
+            { metaLink.universe }
+        </p>
+        <VSpacerComponent space={.2} />
+
+        <p className="text_6 clipped_text">
+          { metaLink.bio }
+        </p>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default MetalinkCardComponent
