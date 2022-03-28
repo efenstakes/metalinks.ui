0056808a891d2d97d9846daee0a9318a6d30e9d3
Efen Stakes <efenstakes101@gmail.com>
2022-03-28 17:26:22 +0300

    refactor code

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
+
 // components
 import AppbarComponent from '../../components/appbar/appbar.component'
 import MetalinkCardComponent from '../../components/metalink_card/metalink_card.component'
 import SectionTitleComponent from '../../components/section_title/section_title.component'
 import WelcomeComponent from './welcome.component'
+import FabComponent from '../../components/fab/fab.component'
+import LoadingMetalinkCardComponent from '../../components/loading_metalink_card/loading_metalink_card.component'
 
 
 // models
@@ -12,12 +16,15 @@ import { avatars } from '../../models/test.data'
 
 
 import './home.page.scss'
-import FabComponent from '../../components/fab/fab.component'
 
 
 const HomePage = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     console.log("avatars ", avatars)
+
+
+    const toggleDrawer = ()=> {}
 
     return (
         <div className='page'>
@@ -31,7 +38,7 @@ const HomePage = () => {
             {/* metalinks if any */}
             <SectionTitleComponent title='Links' />
             <div className="padded_container">
-                {
+                { ! isLoading &&
                     avatars[0].links.map((metaLink: MetaLink, index: number)=> {
 
                         return (
@@ -42,12 +49,23 @@ const HomePage = () => {
                         )
                     })
                 }
+                { isLoading &&
+                    Array(6).fill(0).map((_, index: number)=> {
+
+                        return (
+                            <LoadingMetalinkCardComponent
+                                animationIndex={index+12+2}
+                            />
+                        )
+                    })
+                }
             </div>
+
 
             {/* add link button */}
             <FabComponent
                 children={
-                    <button className="primary_button">
+                    <button className="primary_button" onClick={toggleDrawer}>
                         Add a MetaLink
                     </button>
                 }
