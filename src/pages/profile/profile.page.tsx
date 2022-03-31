f37400d3413be1328c0b36be717e624509f1a224
Efen Stakes <efenstakes101@gmail.com>
2022-03-31 18:01:53 +0300

    add loading state for profile page

--- src/pages/profile/profile.page.tsx
+++ src/pages/profile/profile.page.tsx
@@ -1,6 +1,9 @@
-import { Drawer } from '@mui/material'
-import { profile } from 'console'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useParams, Navigate } from 'react-router-dom'
+
+
+import { Drawer, Skeleton } from '@mui/material'
+
 
 
 // components
@@ -18,8 +21,8 @@ import { MetaLink } from '../../models/metalink.model'
 import { Avatar } from '../../models/avatar.model'
 
 
-// data
-import { avatars } from '../../models/test.data'
+// services
+import { getAvatarByAddress, getAvatarById } from '../../services/metalinks.api.services'
 
 
 // styles
@@ -27,16 +30,119 @@ import './profile.page.scss'
 
 
 const ProfilePage = () => {
+    let { id } = useParams()
+    
     const [isLoading, setIsLoading] = useState<boolean>(false)
     
     const [isAddLinkDrawerOpen, setIsAddLinkDrawerOpen] = useState<boolean>(false)
     const [isCreateAvatarDrawerOpen, setIsCreateAvatarDrawerOpen] = useState<boolean>(false)
 
+    const [avatar, setAvatar] = useState<Avatar|null>()
+    // const profile: Avatar = avatars[0]
 
-    const profile: Avatar = avatars[0]
+
+    // get avatar details - avatar & metalinks
+    useEffect(()=> {
+      getAvatarDetails()
+    }, [ id ])
+
+
+
+    const getAvatarDetails = ()=> {
+      if( id && isNaN(parseInt(id)) ) {
+        console.log("get profile with addess ", id)
+        getByAddress(id)
+      } else {
+        console.log("get profile by id ", id)
+        getByID(id)
+      }
+    }
+    
+    const getByAddress = async (id: string)=> {
+      setIsLoading(true)
+      const avatr: Avatar = await getAvatarByAddress(id) 
+      setAvatar(avatr)
+      setIsLoading(false)
+    }
+    
+    const getByID = async (id: string | number)=> {
+      setIsLoading(true)
+      const avatr: Avatar = await getAvatarById(id) 
+      setAvatar(avatr)
+      setIsLoading(false)
+    }
+
 
     const toggleDrawer = ()=> {}
 
+
+
+
+
+    // if no id, go to home page
+    if( !id ) {
+      <Navigate to="/" />
+    }
+    if( isLoading ) {
+      return (  
+        <div className='page'>
+                      
+          {/* appbar */}
+          <AppbarComponent />
+
+          {/* avatars */}
+          <div className="profile_avatars_container">
+
+            <Skeleton 
+              className="profile_avatars_container__avatar_container__big_avatar absolute"
+              width="100%" height={400}
+            />
+
+            <div className="profile_avatars_container__avatar_container absolute row ma_center ca_center">
+              <div
+                className="profile_avatars_container__avatar_container__avatar__loading" 
+              />
+            </div>
+
+          </div>
+          <VSpacerComponent space={1} />
+
+          <div className="column ca_center profile_info_container">
+
+            <Skeleton width="48%" height={20} />
+            <VSpacerComponent space={.5} />
+
+            <Skeleton width="32%" height={16} />
+            <VSpacerComponent space={1.5} />
+
+            <div className="row ma_evenly ca_center profile_info_container__chips">
+
+              <Skeleton width="30%" height={16} />
+              <Skeleton width="30%" height={16} />
+
+            </div>
+
+          </div>
+          <VSpacerComponent space={6} />
+
+          {/* metalinks if any */}
+          <SectionTitleComponent title='Links' />
+          <div className="padded_container">
+              { 
+                  Array(6).fill(0).map((_, index: number)=> {
+
+                      return (
+                          <LoadingMetalinkCardComponent
+                              animationIndex={index+12+2}
+                          />
+                      )
+                  })
+              }
+          </div>
+
+        </div>
+      )
+    }
     return (
       <div className='page'>
                   
@@ -49,13 +155,13 @@ const ProfilePage = () => {
             <div 
               className="profile_avatars_container__avatar_container__big_avatar absolute"
               style={{
-                backgroundImage: `url(${profile.bg_avatar})`,
+                backgroundImage: `url(${avatar.bg_avatar})`,
               }}
             />
 
             <div className="profile_avatars_container__avatar_container absolute row ma_center ca_center">
               <img 
-                src={ profile.avatar } 
+                src={ avatar.avatar } 
                 alt="avatar" 
                 className="profile_avatars_container__avatar_container__avatar" 
               />
@@ -67,22 +173,22 @@ const ProfilePage = () => {
           <div className="column ca_center profile_info_container">
 
             <p className="text_3 bold">
-              { profile.name }
+              { avatar.name }
             </p>
             <VSpacerComponent space={.5} />
 
             <p className="text_7 profile_info_container__bio">
-              { profile.bio }
+              { avatar.bio }
             </p>
             <VSpacerComponent space={1.5} />
 
             <div className="row ma_evenly ca_center profile_info_container__chips">
 
               <div className="chip_md chip_primary_outlined">
-                4 Links
+                { avatar?.links.length } Links
               </div>
               <div className="chip_md chip_primary_outlined">
-                4 Universes
+                { avatar?.links.length } Universes
               </div>
 
             </div>
@@ -94,22 +200,12 @@ const ProfilePage = () => {
           <SectionTitleComponent title='Links' />
           <div className="padded_container">
               { ! isLoading &&
-                  profile.links.map((metaLink: MetaLink, index: number)=> {
+                  avatar?.links.map((metaLink: MetaLink, index: number)=> {
 
                       return (
                           <MetalinkCardComponent
                               key={index}
                               metaLink={metaLink}
-                          />
-                      )
-                  })
-              }
-              { isLoading &&
-                  Array(6).fill(0).map((_, index: number)=> {
-
-                      return (
-                          <LoadingMetalinkCardComponent
-                              animationIndex={index+12+2}
                           />
                       )
                   })
