b97f921023777c6f6587e8b90ba7e6cd867495af
Efen Stakes <efenstakes101@gmail.com>
2022-04-03 17:54:43 +0300

    add my progile page to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import {
   BrowserRouter as Router, Routes, Route,
 } from "react-router-dom"
 
-
 // pages
 import HomePage from "./pages/home/home.page"
+import MyProfilePage from "./pages/my_profile/my_profile.page"
 import ProfilePage from "./pages/profile/profile.page"
 
 
@@ -13,6 +13,8 @@ function App() {
   return (
     <Router>
       <Routes>
+        
+        <Route path="/me" element={<MyProfilePage />} />
         
         <Route path="/profile/:id" element={<ProfilePage />} />
         
