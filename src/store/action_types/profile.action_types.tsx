7f153b39916936ac6b0939f91cc51c36fc7b4e6b
Efen Stakes <efenstakes101@gmail.com>
2022-04-03 17:48:51 +0300

    add profile redux action types

---
+++ src/store/action_types/profile.action_types.tsx
@@ -0,0 +1,6 @@
+
+
+
+export const SET_CURRENT_AVATAR: string = "ACTIONS/SET/CURRENT_AVATAR"
+export const GET_CURRENT_AVATAR: string = "ACTIONS/GET/CURRENT_AVATAR"
+export const LOGOUT: string = "ACTIONS/LOGOUT"
