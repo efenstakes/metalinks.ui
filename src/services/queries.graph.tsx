1dfad2ed8414e9b5ce2024acd1f54b7dd5054ab9
Efen Stakes <efenstakes101@gmail.com>
2022-04-03 17:51:45 +0300

    add queries for the graph api

---
+++ src/services/queries.graph.tsx
@@ -0,0 +1,62 @@
+import { gql } from "@apollo/client"
+
+
+export const GET_AVATAR_DETAILS_BY_ID_QUERY = gql`
+    query getAvatarByID($id: ID!) {
+        avatars( id: $id ) {
+            id
+            name
+            aka
+            bio
+            avatarURI
+            bgAvatarURI
+            addresses
+            links {
+                id
+                name
+                aka
+                bio
+                universe {
+                    id
+                    name
+                }
+                avatarURI
+                bgAvatarURI
+                link
+            }
+        }
+    }
+`
+
+export const GET_AVATAR_DETAILS_BY_ADDRESS_QUERY = gql`
+    query getAvatarProfile($address: String!) {
+        avatars(
+            where: {
+                addresses_contains: [
+                    $address
+                ]
+            }
+        ) {
+            id
+            name
+            aka
+            bio
+            avatarURI
+            bgAvatarURI
+            addresses
+            links {
+                id
+                name
+                aka
+                bio
+                universe {
+                    id
+                    name
+                }
+                avatarURI
+                bgAvatarURI
+                link
+            }
+        }
+    }
+`
