febfd1d686cd7692cd7f3c36daa142efb376951e
Efen Stakes <efenstakes101@gmail.com>
2022-04-04 15:32:21 +0300

    add component to show when avatar has no metalinks yet

---
+++ src/components/no_links/no_links.component.tsx
@@ -0,0 +1,33 @@
+import VSpacerComponent from '../v_spacer/v_spacer.component'
+
+// assets
+import face_img from '../../assets/images/face-big.png'
+
+
+import './no_links.component.scss'
+
+
+type ComponentProps = {
+    text: string
+}
+const NoLinksComponent = ({ text }: ComponentProps) => {
+  return (
+    <div className='no_links_container column ca_center'>
+        
+        <img src={face_img} className='no_links_container__image' />
+        <VSpacerComponent space={1} />
+        
+        <p className="text_3 bold no_links_container__title">
+            No MetaLinks
+        </p>
+        <VSpacerComponent space={.5} />
+        
+        <p className="text_6 no_links_container__text text_center">
+            { text }
+        </p>
+
+    </div>
+  )
+}
+
+export default NoLinksComponent
