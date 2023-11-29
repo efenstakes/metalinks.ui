7f80d47665b7c51c7074dcda3faf94c8c0491b6f
efen stakes <efenstakes101@gmail.com>
2023-11-29 16:10:14 +0300

    update graphql logic to test

--- src/pages/profile/profile.page.tsx
+++ src/pages/profile/profile.page.tsx
@@ -54,12 +54,12 @@ const ProfilePage = () => {
     
     // query the graph for profile
     const qParams: ByID | ByAddress = isID ? { id: id as string } : { address: id }
-    const { loading, error, data, refetch } = useQuery(
-      isID ? GET_AVATAR_DETAILS_BY_ID_QUERY : GET_AVATAR_DETAILS_BY_ADDRESS_QUERY,
-      {
-        variables: qParams
-      }
-    )
+    // const { loading, error, data, refetch } = useQuery(
+    //   isID ? GET_AVATAR_DETAILS_BY_ID_QUERY : GET_AVATAR_DETAILS_BY_ADDRESS_QUERY,
+    //   {
+    //     variables: qParams
+    //   }
+    // )
 
     useEffect(()=> {
       refreshProfile()
@@ -70,168 +70,174 @@ const ProfilePage = () => {
       const params: ByID | ByAddress = isID ? { id: id as string } : { address: id as string }
       // refetch()
       console.log("b4 refetch({ ...params })", params)
-      refetch({ ...params })
+      // refetch({ ...params })
       console.log("after refetch({ ...params })", params)
     }
 
-
-    console.log("loading, error, data ", loading, error, data)
-    // if no id, go to home page
-    if( !id ) {
-      return (
-        <Navigate to="/" />
-      )
-    }
-    if( loading ) {
-      return (
-        <ProfileLoadingPage />
-      )
-    }
-
-    // error account
-    if( error ) {
-        console.log("error account")
-        return (
-            <div className='page'>
-            
-                {/* appbar */}
-                <AppbarComponent />
-                <VSpacerComponent space={10} />
-
-                <div className="padded_container_lg">
-                    <ErrorLoadingAvatarComponent refresh={refreshProfile} />
-                </div>
-
-            </div>
-        )
-    }
-    
-    // no error but no account either
-    if(
-        !error &&
-        (
-          !data ||
-          (isID && !data?.avatar) || 
-          (!isID && !data?.avatars) || 
-          (!isID && data?.avatars?.length === 0)
-        )
-    ) {
-        console.log("no error but no account either")
-        return (
-            <div className='page'>
-            
-                {/* appbar */}
-                <AppbarComponent />
-                <VSpacerComponent space={10} />
-
-                <div className="padded_container_lg">
-                    <ErrorLoadingAvatarComponent 
-                        title="Avatar Not Found"
-                        text="Avatar you are searching was not found. Check the id or address."
-                        refresh={refreshProfile} 
-                        hideCta
-                    />
-                </div>
-
-            </div>
-        )
-    }
-
-    // get the avatar from result
-    const avatar: Avatar = toAvatar(
-      isID ? data?.avatar : data?.avatars[0]
-    )
     return (
-      <div className='page'>
-                  
-          {/* appbar */}
-          <AppbarComponent />
-
-          {/* avatars */}
-          <div className="profile_avatars_container">
-
-            <div 
-              className="profile_avatars_container__avatar_container__big_avatar absolute"
-              style={{
-                backgroundImage: `url(${avatar.bg_avatar})`,
-              }}
-            />
-
-            <div className="profile_avatars_container__avatar_container absolute row ma_center ca_center">
-              <img 
-                src={ avatar.avatar } 
-                alt="avatar" 
-                className="profile_avatars_container__avatar_container__avatar su_2" 
-              />
-            </div>
-
-          </div>
-          <VSpacerComponent space={1} />
-
-          <div className="column ca_center profile_info_container">
-
-            <p className="text_3 bold su_4">
-              { avatar?.name }
-            </p>
-            <VSpacerComponent space={.5} />
-
-            <p className="text_7 profile_info_container__bio su_8">
-              { avatar?.bio }
-            </p>
-            <VSpacerComponent space={1.5} />
-
-            {/* meta info */}
-            <div className="row ma_evenly ca_center profile_info_container__chips">
-
-              {/* Links */}
-              <div className="chip_md chip_primary_outlined text_6 su_11">
-                { avatar?.links.length } Links
-              </div>
-
-              {/* Universes */}
-              <div className="chip_md chip_primary_outlined text_6 su_13">
-                { avatar?.links.length } Universes
-              </div>
-
-              {/* Addresses */}
-              <div className="chip_md chip_primary_outlined text_6 su_16">
-                  { avatar?.addresses.length } Addresses
-              </div>
-
-            </div>
-
-          </div>
-          <VSpacerComponent space={6} />
-
-          {/* metalinks if any */}
-          {  
-            avatar?.links.length > 0 && 
-              <SectionTitleComponent title='Links' /> 
-          }
-          <div className="padded_container_lg">
-              
-              {
-                  avatar?.links.map((metaLink: MetaLink, index: number)=> {
-
-                      return (
-                          <MetalinkCardComponent
-                              key={index}
-                              metaLink={metaLink}
-                              animationIndex={index+16+2}
-                          />
-                      )
-                  })
-              }
-              {
-                  avatar && avatar?.links.length === 0 &&
-                      <NoLinksComponent
-                          text={`${avatar?.name} has not added any MetaLinks yet.`}
-                      />
-              }
-          </div>
-            
+      <div>
+        {qParams}
       </div>
     )
+
+
+    // console.log("loading, error, data ", loading, error, data)
+    // if no id, go to home page
+    // if( !id ) {
+    //   return (
+    //     <Navigate to="/" />
+    //   )
+    // }
+    // if( loading ) {
+    //   return (
+    //     <ProfileLoadingPage />
+    //   )
+    // }
+
+    // error account
+    // if( error ) {
+    //     console.log("error account")
+    //     return (
+    //         <div className='page'>
+            
+    //             {/* appbar */}
+    //             <AppbarComponent />
+    //             <VSpacerComponent space={10} />
+
+    //             <div className="padded_container_lg">
+    //                 <ErrorLoadingAvatarComponent refresh={refreshProfile} />
+    //             </div>
+
+    //         </div>
+    //     )
+    // }
+    
+    // no error but no account either
+    // if(
+    //     !error &&
+    //     (
+    //       !data ||
+    //       (isID && !data?.avatar) || 
+    //       (!isID && !data?.avatars) || 
+    //       (!isID && data?.avatars?.length === 0)
+    //     )
+    // ) {
+    //     console.log("no error but no account either")
+    //     return (
+    //         <div className='page'>
+            
+    //             {/* appbar */}
+    //             <AppbarComponent />
+    //             <VSpacerComponent space={10} />
+
+    //             <div className="padded_container_lg">
+    //                 <ErrorLoadingAvatarComponent 
+    //                     title="Avatar Not Found"
+    //                     text="Avatar you are searching was not found. Check the id or address."
+    //                     refresh={refreshProfile} 
+    //                     hideCta
+    //                 />
+    //             </div>
+
+    //         </div>
+    //     )
+    // }
+
+    // get the avatar from result
+    // const avatar: Avatar = toAvatar(
+    //   isID ? data?.avatar : data?.avatars[0]
+    // )
+    // return (
+    //   <div className='page'>
+                  
+    //       {/* appbar */}
+    //       <AppbarComponent />
+
+    //       {/* avatars */}
+    //       <div className="profile_avatars_container">
+
+    //         <div 
+    //           className="profile_avatars_container__avatar_container__big_avatar absolute"
+    //           style={{
+    //             backgroundImage: `url(${avatar.bg_avatar})`,
+    //           }}
+    //         />
+
+    //         <div className="profile_avatars_container__avatar_container absolute row ma_center ca_center">
+    //           <img 
+    //             src={ avatar.avatar } 
+    //             alt="avatar" 
+    //             className="profile_avatars_container__avatar_container__avatar su_2" 
+    //           />
+    //         </div>
+
+    //       </div>
+    //       <VSpacerComponent space={1} />
+
+    //       <div className="column ca_center profile_info_container">
+
+    //         <p className="text_3 bold su_4">
+    //           { avatar?.name }
+    //         </p>
+    //         <VSpacerComponent space={.5} />
+
+    //         <p className="text_7 profile_info_container__bio su_8">
+    //           { avatar?.bio }
+    //         </p>
+    //         <VSpacerComponent space={1.5} />
+
+    //         {/* meta info */}
+    //         <div className="row ma_evenly ca_center profile_info_container__chips">
+
+    //           {/* Links */}
+    //           <div className="chip_md chip_primary_outlined text_6 su_11">
+    //             { avatar?.links.length } Links
+    //           </div>
+
+    //           {/* Universes */}
+    //           <div className="chip_md chip_primary_outlined text_6 su_13">
+    //             { avatar?.links.length } Universes
+    //           </div>
+
+    //           {/* Addresses */}
+    //           <div className="chip_md chip_primary_outlined text_6 su_16">
+    //               { avatar?.addresses.length } Addresses
+    //           </div>
+
+    //         </div>
+
+    //       </div>
+    //       <VSpacerComponent space={6} />
+
+    //       {/* metalinks if any */}
+    //       {  
+    //         avatar?.links.length > 0 && 
+    //           <SectionTitleComponent title='Links' /> 
+    //       }
+    //       <div className="padded_container_lg">
+              
+    //           {
+    //               avatar?.links.map((metaLink: MetaLink, index: number)=> {
+
+    //                   return (
+    //                       <MetalinkCardComponent
+    //                           key={index}
+    //                           metaLink={metaLink}
+    //                           animationIndex={index+16+2}
+    //                       />
+    //                   )
+    //               })
+    //           }
+    //           {
+    //               avatar && avatar?.links.length === 0 &&
+    //                   <NoLinksComponent
+    //                       text={`${avatar?.name} has not added any MetaLinks yet.`}
+    //                   />
+    //           }
+    //       </div>
+            
+    //   </div>
+    // )
 }
 
 export default ProfilePage
